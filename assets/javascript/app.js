c4da78ecb61ced5269167767e3c3d9da138f6a0c
k2k <kadonna@wonderkul.com>
2019-06-22 17:49:48 -0500

    basic page layout completed

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+//The Trivia Game - My javascript file //
+$(document).ready(function(){
+
+//Assign the correct answer to each answer choice
+//Timer countsdown from 30 seconds to zero
+//when timer is at zero, stop the timer and evaluate if answers are correct
+//when all questions have been answered evaluate if answers are correct or incorrect 
+//evaluate & display how many incorrect answers were input
+
+
+
+
+
+
+
+
+
+
+});
